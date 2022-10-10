24366eef2c1aa6ab6f3c6d6bf283bbfa48ce87a8
Trefania <tarirotrefania@gmail.com>
2022-10-10 04:39:02 -0700

    prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,11 @@
 def safe_print_list(my_list=[], x=0):
     a = 0
     for i in range(x):
+
         try:
-            print(f"my_list[i]", end"")
+            print(my_list[i], end"")
             a += 1
         except IndexError:
             break
-        return(a)
     print()
+    return(a)
