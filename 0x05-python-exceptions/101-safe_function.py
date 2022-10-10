e04d0d1fc29e6f281cf7a7957bd3f44d7b0681c3
Trefania <tarirotrefania@gmail.com>
2022-10-10 12:23:52 -0700

    executes a function safely.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
     try:
-        a = fct(*args)
+        a= fct(*args)
         return a
-    except Exception as error:
-        print("Exception: {}".format(error_m), file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
