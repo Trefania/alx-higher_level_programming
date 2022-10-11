36c856936aef90ef93c1afed5562d0fdc77f8352
Trefania <tarirotrefania@gmail.com>
2022-10-11 10:30:42 -0700

    square size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
-sfffadsw
+#!/usr/bin/python3
+"""Defs a class Square"""
+
+
+class Square:
+    """showing a square"""
+    def __init__(self, size):
+        """square class is being initialized
+        size - is an arguement that is being passed
+        """
+        self.__size = size
