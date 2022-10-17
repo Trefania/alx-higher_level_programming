55dc8289364d7109ad110eb0ed31fe3433a13714
Trefania <tarirotrefania@gmail.com>
2022-10-17 04:38:19 -0700

    simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    def __init___(self, size):
+        """rectangle class is being initialized
+        size - is an arguement that is being passed
+        """
+        self.__size = size
