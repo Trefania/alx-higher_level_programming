74e2d2ff286ebd83ac37329bf0dfe594958ff351
Trefania <tarirotrefania@gmail.com>
2022-10-21 14:45:32 -0700

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,10 +7,9 @@ def add_integer(a, b=98):
     floats aare going to be typecasted to integer be4 adding to two.
     Error message is being raise if both are not floats or integer.
     """
-    addition = int a + int b
-
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
+    addition = int (a) + int (b)
     return (addition)
