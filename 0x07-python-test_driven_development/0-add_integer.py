260e34dec73b11f18a5e9b79ff8385155f642a50
Trefania <tarirotrefania@gmail.com>
2022-10-21 14:46:46 -0700

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,5 +11,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    addition = int (a) + int (b)
+    addition = int(a) + int(b)
     return (addition)
