37eeba3235331bf92a0d19d1c9511df9d2c0b0c2
Trefania <107484364+Trefania@users.noreply.github.com>
2022-12-19 23:27:06 +0200

    constant

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+let first_line = "C is fun";
+let sec_line = "Python is cool";
+let third_line = "JavaScript is amazing";
+
+console.log(first_line, sec_line, third_line);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let arg = 0;
+if (arg = 0) {
+	console.log("No argument");
+}
+else if (arg = 1) {
+	console.log("Argument found");
+}
+else {
+	console.log("Arguments found");
+}
