e5c413fc66e1e9654dda5a169b2255b68449d175
Trefania <107484364+Trefania@users.noreply.github.com>
2022-12-21 12:31:31 +0200

    loop to languages

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-/**
- * * A Script that prints the first argument passed to it 
- * If no arguments are passed to the script, print “No argument
- */
-
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
-  console.log('No argument');
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
+/* Declaring an array of languages */
 const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
 for (const myArr of languages) {
+  /* print the output */
   console.log(myArr);
 }
