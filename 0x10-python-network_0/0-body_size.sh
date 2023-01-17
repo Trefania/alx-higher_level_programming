23ae1cd21a752f1eca7e611ff519e9855ca9460a
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-17 12:55:21 +0200

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -sX "$1" | wc -c
+#curl -sX "$1" | wc -c
+curl -s -I $1 | awk '/^Content-Length/ { print $2 }'
