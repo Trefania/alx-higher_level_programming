83f51a1fef6c030498057cb18b204aafe7fd824e
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-18 14:03:34 +0200

    sstatus

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    #req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    #with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         #reading the content of the response as html var
         html = response.read()
         #printing html content
