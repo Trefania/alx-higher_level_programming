0d14c34b332976cbe627adc6da31e930dc21e5b8
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-18 12:15:21 +0200

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# script that fetches https://alx-intranet.hbtn.io/status
+# You must use the package urllib
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        #reading the content of the response as html var
+        html = response.read()
+        #printing html content
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
