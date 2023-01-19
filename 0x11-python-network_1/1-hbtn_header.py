49202712a66d9d7562940bf72f3fb3b6f7081df5
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-19 16:57:04 +0200

    email

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Sends a request to the URL and displays the value of the X-Request-Id variable found
-in the header of the response.
+"""Sends a request to the URL and displays the value of 
+the X-Request-Id variable found in the header of the response.
 """
 import urllib.request
 import sys
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+""" takes in a URL and an email, sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response (decoded in utf-8)
+"""
 import urllib.request
 import urllib.parse
 from sys import argv
