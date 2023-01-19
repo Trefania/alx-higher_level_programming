950a3fa409669c97b4e176fd3372d7cac40162b0
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-19 13:25:02 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ You must use the package urllib"""
 import urllib.request
 
 if __name__ == "__main__":
-    #req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    #with urllib.request.urlopen(req) as response:
+# req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    # with urllib.request.urlopen(req) as response:
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        #reading the content of the response as html var
+        # reading the content of the response as html var
         html = response.read()
-        #printing html content
+        # printing html content
         print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
