f2aa2d1dd9b2047d41605cd9cd6a4e659df65883
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-19 22:16:55 +0200

    header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""script that fetches https://alx-intranet.hbtn.io/status
-You must use the package urllib"""
+"""
+A Python Script that fetches 'https://alx-intranet.hbtn.io/status'
+"""
 import urllib.request
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        # with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        # reading the content of the response as html var
-        html = response.read()
-        # printing html content
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
         print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Sends a request to the URL and displays the value of 
-the X-Request-Id variable found in the header of the response.
+"""
+A Script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response.
 """
 import urllib.request
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    URL = sys.argv[1]
-    with urllib.request.urlopen(URL) as response:
-        X_Request_d = response.headers.get("X-Request-Id")
-        print(f"{X_Request_d}")
+    request = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(request) as resp:
+        value = dict(resp.headers).get('X-Request-Id')
+        print(value)
