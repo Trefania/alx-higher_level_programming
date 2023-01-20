8c30b0b3936ffdf0afed1caa05459ecd512ed1ff
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-20 23:02:06 +0200

    code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,12 +3,9 @@
   - Handles HTTP errors.
 """
 
-
-
 if __name__ == "__main__":
     from sys import argv
     import requests
-
     r = requests.get(argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
