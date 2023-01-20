015657fe4f5b6a020e409a36043e5efd7f555175
Trefania <tarirotrefania@gmail.com>
2023-01-20 10:28:36 -0800

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
+"""
+Sends a POST request to a given URL with a given email
   - Displays the body of the response.
 """
 import sys
