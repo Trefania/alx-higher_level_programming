0a5474fda839ba1320e3902c915ff0900f07999e
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-20 12:11:22 +0200

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""a Python script that takes in a URL and an email,
+   sends a POST request to the passed URL
 """
 import sys
 import urllib.parse
@@ -16,4 +15,3 @@ if __name__ == "__main__":
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
-
