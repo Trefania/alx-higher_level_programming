0894efa2c6f4328255b8d9000b20cf7090c067ab
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-20 13:13:21 +0200

    code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Sends a request to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
   - Handles HTTP errors.
 """
-import sys
-import requests
+
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    from sys import argv
+    import requests
 
-    r = requests.get(url)
+    r = requests.get(argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
