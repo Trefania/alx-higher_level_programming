b6c7c81b3f06f7b93dc40c6b1aeb81568162b2eb
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-20 19:57:59 +0200

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ a Python script that takes in a URL, sends a request to the URL
 if __name__ == "__main__":
     import urllib.request as request
     from sys import argv
+
     req = request.Request(argv[1])
     with request.urlopen(req) as r:
         print(r.headers.get('X-Request-Id'))
